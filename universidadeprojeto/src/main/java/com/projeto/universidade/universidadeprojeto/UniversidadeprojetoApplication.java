2d5221b48dd716473ee00b0b33661b193e299e60
matheus.pinho <matheus.pinho@azi.com.br>
2020-11-27 15:57:53 -0400

    Adicionando Liquibase ao projeto.

---
+++ universidadeprojeto/src/main/java/com/projeto/universidade/universidadeprojeto/UniversidadeprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.universidade.universidadeprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversidadeprojetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversidadeprojetoApplication.class, args);
+    }
+
+}
---
+++ universidadeprojeto/src/main/resources/changelog/database/postgresql/001-estrutura-inicial.sql
@@ -0,0 +1,8 @@
+-- TABLE ALUNO -------------------------------
+
+CREATE TABLE "universidade"."tb_aluno"
+
+(
+    "aluno_id" Integer NOT NULL,
+    "aluno_nome" Character varying (255)
+)
